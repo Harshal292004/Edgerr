ead91ff58121d9c9a8ca909f99e79e5de6beaa06
Harshal292004 <malaniharshal95@gmail.com>
2025-03-22 15:27:45 +0530

    Add Multiple data base support

--- PydanticModels.py
+++ PydanticModels.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field
 from typing import TypedDict, Optional, Dict,Annotated ,List ,Literal
-from typing import TypedDict, Optional, Dict,Annotated ,List ,Literal
 from langgraph.graph import StateGraph ,START,END
 from langchain_openai import ChatOpenAI
 from langchain_groq import  ChatGroq
@@ -10,7 +9,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import  MessagesState
 from langchain_core.messages import HumanMessage,SystemMessage ,AnyMessage, AIMessage
 from pydantic import  BaseModel,Field
-
+from enum import Enum
 
 # CODD MODELS
 class Decision(BaseModel):
@@ -44,9 +43,17 @@ class OptimizedQuery(BaseModel):
 class FinalEvaluation(BaseModel):
     evaluation: Annotated[bool, Field(description="Is the query appropriate for the SQL prompt and context")]
 
+class DB_TYPES(Enum):
+    POSTGRESS:"postgress"
+    MYSQL:"mysql"
+    MARIADB:"mariadb"
+class DBConfigurations(BaseModel):
+    db_type:Optional[Annotated[DB_TYPES, Field(description="Type fo the database")]] 
+    db_connection_str:Optional[Annotated[str,Field(description="Connection string for database")]]
+    
 class StoneBreakerState(BaseModel):
-    data_base: Optional[Annotated[str, Field(description="Database connection path")]] = None
-    sql_context: Optional[Annotated[str, Field(description="SQL schema context")]] = None
+    db_configurations: Optional[Annotated[DBConfigurations,Field(description="Database configurations")]]
+    sql_context: Optional[Annotated[str, Field(description="SQL schema")]] = None
     sql_prompt: Optional[Annotated[str, Field(description="User SQL query prompt")]] = None
     sql_context_from_vector_store: Optional[Annotated[str, Field(description="SQL context retrieved from vector store")]] = None
     sql_query_generated: Optional[Annotated[str, Field(description="Generated SQL query")]] = None
---
+++ Utils.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+class DatabaseConnection:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def build_connection_string(config:dict)->str:
+        db_type=config.get("db_type").lower()
+        if db_type == "sqlite":
+            db_path=config.get("db_path")
+            return f"sqlite:///{db_path}"
+        
+        username=config.get("username")
+        password=config.get("password")
+        host= config.get("host")
+        port= config.get("port")
+        dbname= config.get("dbname")
+        
+        if db_type=="postgress":
+            return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}"
+        elif db_type=="mysql":
+            return f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}"
+        elif db_type == "mariadb":
+            # MariaDB using the mariadbconnector
+            return f"mysql+mariadbconnector://{username}:{password}@{host}:{port}/{dbname}"
+        else:
+            raise ValueError("Unsupported database type provided.")
+    
+    @staticmethod
+    def connect_to_database(config:dict):
+        try:
+            conn_str = DatabaseConnection.build_connection_string(config)
+            engine = create_engine(conn_str)
+            with engine.connect() as connection:
+                print("Connection Successful!")
+            return engine
+        except SQLAlchemyError as e:
+            print(f"Failed to connect: {e}")
+            return None
+        
